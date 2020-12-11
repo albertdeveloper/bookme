5b1c411ca54467e657e8dcd6a6f26d56b089cec5
albertdeveloper <rossalbertgonzales@gmail.com>
2020-12-12 01:26:26 +0800

    add room type test

---
+++ tests/Feature/Room/RoomTypeTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature\Room;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RoomTypeTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $response = $this->get('/room_type');
+
+        $response->assertStatus(200);
+    }
+}

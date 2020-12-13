a27bc72023e8dcd130b142a62f303a51ed5b2d74
albertdeveloper <rossalbertgonzales@gmail.com>
2020-12-14 00:51:51 +0800

    update room type

--- database/seeders/RoomTypeSeeder.php
+++ database/seeders/RoomTypeSeeder.php
@@ -18,19 +18,31 @@ class RoomTypeSeeder extends Seeder
      */
     public function run()
     {
-        $faker =  Factory::create();
+
         $user = User::first();
 
         DB::table('rooms')->delete();
         DB::table('room_types')->delete();
 
-        for($i = 0; $i < 100; $i++) {
-            $company = $faker->company;
+
+        $room_type = array(
+            'Single' => 'A room assigned to one person. May have one or more beds.',
+            'Double' => 'A room assigned to two people. May have one or more beds.',
+            'Triple' => 'A room assigned to three people. May have two or more beds.',
+            'Quad' => 'A room assigned to four people. May have two or more beds.',
+            'Queen' => 'A room with a queen-sized bed. May be occupied by one or more people.',
+            'King' => 'A room with a king-sized bed. May be occupied by one or more people.',
+            'Twin' => 'A room with two beds. May be occupied by one or more people.',
+            'Double-double' => 'A room with two double (or perhaps queen) beds. May be occupied by one or more people.',
+            'Studio' => 'A room with a studio bed – a couch that can be converted into a bed. May also have an additional bed',
+        );
+        foreach($room_type as  $title => $desc) {
+
             RoomType::create([
                 'user_id' => $user->id,
-                'title' => $company,
-                'slug' => $company,
-                'description' => $faker->sentence,
+                'title' => $title,
+                'slug' => $title,
+                'description' => $desc,
             ]);
         }
 
